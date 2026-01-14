163e85620093457146fdc4e93b2e4cda2b40f749
Esteban Rojas Huerta <esteban_moreeste@hotmail.com>
2026-01-13 22:58:57 -0600

    Add FlagEntity and Flags DbSet to ApplicationDbContext
    Create flag entity

--- src/FlagX0.Web/Data/ApplicationDbContext.cs
+++ src/FlagX0.Web/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FlagX0.Web.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FlagX0.Web.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        public DbSet<FlagEntity> Flags { get; set; }
     }
 }
